25a049e6064fb80c7471b8297e19ffca306f10e3
whozprime <mateustururu01@gmail.com>
2025-06-14 04:20:04 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('CryptoBot Webhook online!');
+});
+
+app.post('/webhook', (req, res) => {
+  console.log('Recebi webhook:', req.body);
+
+  // Aqui você processa a notificação do CryptoBot
+  // Por exemplo, checar se o pagamento foi confirmado
+
+  res.status(200).send('Webhook recebido com sucesso');
 });
 
 const PORT = process.env.PORT || 3000;
